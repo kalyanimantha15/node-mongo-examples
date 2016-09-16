eba8f8f24fc86fa2f68920da1b53f5ddc258022d
kalyanimantha15 <kalyani.m@motivitylabs.com>
2016-09-16 18:52:04 +0530

    Add files via upload

---
+++ RegExpExample.js
@@ -0,0 +1,20 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+
+
+server.get(/^\/([a-zA-Z0-9_\.~-]+)\/(.*)/, function(req, res, next) {
+  console.log(req.params[0]);
+  console.log(req.params[1]);
+  res.send(200);
+  return next();
+});
+server.head('/hello/:name', respond);
+server.listen(2000, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
