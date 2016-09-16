8c837b9bfd3b9ba1ef507fc0444e26e52a87c0de
kalyanimantha15 <kalyani.m@motivitylabs.com>
2016-09-16 18:47:55 +0530

    Add files via upload

---
+++ routing.js
@@ -0,0 +1,21 @@
+var restify = require('restify');
+
+function send(req, res, next) {
+   res.send('hello ' + req.params.name);
+   return next();
+ }
+
+
+var server = restify.createServer();
+
+ server.post('/hello', function create(req, res, next) {
+   res.send(201, Math.random().toString(36).substr(3, 8));
+   return next();
+ });
+ server.put('/hello', send);
+ server.get('/hello/:name', send);
+ server.head('/hello/:name', send);
+ server.del('hello/:name', function rm(req, res, next) {
+   res.send(204);
+   return next();
+ });
