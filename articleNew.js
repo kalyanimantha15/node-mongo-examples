f59956ceb312534a8f5b1dd6ccac5018dbe302a6
kalyanimantha15 <kalyani.m@motivitylabs.com>
2016-09-19 19:04:09 +0530

    Restify & Mongoose example

---
+++ articleNew.js
@@ -0,0 +1,112 @@
+var mongoose = require('mongoose'),
+    Article = mongoose.model("Article"),
+    ObjectId = mongoose.Types.ObjectId
+ 
+exports.createArticle = function(req, res, next) {
+    var articleModel = new Article(req.body);
+    articleModel.save(function(err, article) {
+        if (err) {
+            res.status(500);
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            })
+        } else {
+            res.json({
+                type: true,
+                data: article
+            })
+        }
+    })
+}
+ 
+exports.viewArticle = function(req, res, next) {
+    Article.findById(new ObjectId(req.params.id), function(err, article) {
+        if (err) {
+            res.status(500);
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            })
+        } else {
+            if (article) {
+                res.json({
+                    type: true,
+                    data: article
+                })
+            } else {
+                res.json({
+                    type: false,
+                    data: "Article: " + req.params.id + " not found"
+                })
+            }
+        }
+    })
+}
+ 
+exports.viewArticle_v2 = function(req, res, next) {
+    Article.findById(new ObjectId(req.params.id), function(err, article) {
+        if (err) {
+            res.status(500);
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            })
+        } else {
+            if (article) {
+                article.title = article.title + " v2"
+                res.json({
+                    type: true,
+                    data: article
+                })
+            } else {
+                res.json({
+                    type: false,
+                    data: "Article: " + req.params.id + " not found"
+                })
+            }
+        }
+    })
+}
+ 
+exports.updateArticle = function(req, res, next) {
+    var updatedArticleModel = new Article(req.body);
+    Article.findByIdAndUpdate(new ObjectId(req.params.id), updatedArticleModel, function(err, article) {
+        if (err) {
+            res.status(500);
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            })
+        } else {
+            if (article) {
+                res.json({
+                    type: true,
+                    data: article
+                })
+            } else {
+                res.json({
+                    type: false,
+                    data: "Article: " + req.params.id + " not found"
+                })
+            }
+        }
+    })
+}
+ 
+exports.deleteArticle = function(req, res, next) {
+    Article.findByIdAndRemove(new Object(req.params.id), function(err, article) {
+        if (err) {
+            res.status(500);
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            })
+        } else {
+            res.json({
+                type: true,
+                data: "Article: " + req.params.id + " deleted successfully"
+            })
+        }
+    })
+}
---
+++ comment.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+var Schema   = mongoose.Schema;
+ 
+var CommentSchema = new Schema({
+    text: String,
+    article: {
+        type: String,
+        ref: "Article"
+    },
+    author: {
+        type: String,
+        ref: "User"
+    }
+});
+mongoose.model('Comment', CommentSchema);
